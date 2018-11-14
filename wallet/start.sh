f8a7de889738a7369ee2da6662b09d98f26a7f13
ricl <riclavaz@gmail.com>
2018-11-14 21:09:51 +0800

    fix wallet script

--- wallet/start.sh
+++ wallet/start.sh
@@ -6,7 +6,12 @@ PID_FILE="wallet.pid"
 WALLET_HOST="127.0.0.1"
 WALLET_PORT="3000"
 
+if [ -f $WALLET_DATA_DIR"/"$PID_FILE ]; then
+    echo "wallet is already running"
+    exit 127
+fi
+
 keosd --config-dir $WALLET_DATA_DIR \
       --wallet-dir $WALLET_DATA_DIR \
       --http-server-address $WALLET_HOST:$WALLET_PORT \
-      $@ & echo $! > $WALLET_DATA_DIR/$PID_FILE
+      & echo $! > $WALLET_DATA_DIR/$PID_FILE
